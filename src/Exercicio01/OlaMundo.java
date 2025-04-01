02720aa5c2a6136280c0314fbadd26554c553851
Leticia-SS <leticia.saraiva013@gmail.com>
2025-04-01 19:58:16 -0300

    exercicio 01

---
+++ src/Exercicio01/OlaMundo.java
@@ -0,0 +1,7 @@
+package Exercicio01;
+
+public class OlaMundo {
+    public static void main(String[] args) {
+        System.out.println("Olá, meu nome é Leticia e estou aprendendo Java");
+    }
+}
