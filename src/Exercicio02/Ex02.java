da03e60dfe3761c1aefc49097b296aaaa070329a
Leticia-SS <leticia.saraiva013@gmail.com>
2025-04-02 00:20:44 -0300

    exercicio 02

--- src/Exercicio02/Ex02.java
+++ src/Exercicio02/Ex02.java
@@ -6,22 +6,33 @@ public class Ex02 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Digite seu nome: ");
+        System.out.print("Digite seu nome: ");
         String name = input.next();
 
-        System.out.println("Digite sua senha: ");
+        System.out.print("Digite sua senha: ");
         String password = input.next();
 
-        boolean validLenght = (password.length() > 8);
-        boolean validNumber = false;
-        boolean validChar = false;
+        boolean isValid = false;
 
-        if (validLenght) {
-            for(var letter : password.toCharArray()) {
-                if (letter >= 0 || letter <= 9) {
-                    validNumber = true;
-                }
+        // Referencia: https://regex101.com/
+        while (!isValid) {
+            if(password.length() < 8){
+                System.out.println("Senha deve ter mais de 8 caracteres\n");
+            } else if (!password.matches(".*[A-Z].*")) {
+                System.out.println("Senha deve conter pelo menos 1 letra maiúscula\n");
+            } else if (!password.matches(".*[0-9].*")) {
+                System.out.println("Senha deve conter pelo menos 1 numero\n");
+            } else if (!password.matches(".*[@#$%&*!].*")) {
+                System.out.println("Senha deve conter pelo menos um caracter especial\n");
+            } else {
+                isValid = true;
+                System.out.println("Senha válida");
+                break;
             }
+
+            System.out.print("Digite novamente: ");
+            password = input.next();
+
         }
     }
 }
