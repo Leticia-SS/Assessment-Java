a1fb518388ee5de019e74e60caa7fef4459d5ad6
Leticia-SS <leticia.saraiva013@gmail.com>
2025-04-03 09:12:20 -0300

    exercicio 05

---
+++ src/Exercicio05/Ex5.java
@@ -0,0 +1,13 @@
+package Exercicio05;
+
+public class Ex5 {
+    public static void main(String[] args) {
+        System.out.println("Content-type: text/html\n");
+        System.out.println("<html>");
+        System.out.println("<head><title>Saudações CGI</title></head>");
+        System.out.println("<body>");
+        System.out.println("<h1>Olá, Terráqueos!</h1>");
+        System.out.println("</body>");
+        System.out.println("</html>");
+    }
+}
